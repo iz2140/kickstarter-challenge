0d09d2e90b2fcd6f8092a78eb620df38bac4839d
Iris <iz2140@columbia.edu>
2015-02-15 20:48:22 -0500

    Initial Commit

---
+++ ListChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ListChallenge
+//
+//  Created by Iris Zhang on 2/15/15.
+//  Copyright (c) 2015 Kickstarter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ListChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ListChallenge
+//
+//  Created by Iris Zhang on 2/15/15.
+//  Copyright (c) 2015 Kickstarter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
